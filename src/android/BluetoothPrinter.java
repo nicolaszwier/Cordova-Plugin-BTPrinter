9a329c1eaf3c1c4c6c2c4dceeeb730bd37649d7c
nicolaszwier <nicolaszwier@gmail.com>
2019-10-25 16:58:11 -0300

    fix

--- src/android/BluetoothPrinter.java
+++ src/android/BluetoothPrinter.java
@@ -566,7 +566,7 @@ public class BluetoothPrinter extends CordovaPlugin {
             // mmOutputStream.write((char) code.length()); // length of encoded string
 
             mmOutputStream.write((char) 73);
-            mmOutputStream.write((char) 46);
+            mmOutputStream.write((char) 10);
             // mmOutputStream.write((char) 123);
             // mmOutputStream.write((char) 66);
             // mmOutputStream.write((char) 78);
@@ -576,9 +576,6 @@ public class BluetoothPrinter extends CordovaPlugin {
             mmOutputStream.write((char) 67);
             mmOutputStream.write(code.getBytes());// d1-dk
             mmOutputStream.write(0);// print barcode
-            // mmOutputStream.write((char) 12);
-            // mmOutputStream.write((char) 34);
-            // mmOutputStream.write((char) 56);
 
             // tell the user data were sent
             Log.d(LOG_TAG, "PRINT BARCODE COMMAND SENT");
