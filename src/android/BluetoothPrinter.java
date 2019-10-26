3e5973f1893fd625bff375c041a92da5332a9841
nicolaszwier <nicolaszwier@gmail.com>
2019-10-26 08:43:38 -0300

    .

--- src/android/BluetoothPrinter.java
+++ src/android/BluetoothPrinter.java
@@ -577,7 +577,7 @@ public class BluetoothPrinter extends CordovaPlugin {
             // mmOutputStream.write((char) 46);
             mmOutputStream.write((char) 123);
             mmOutputStream.write((char) 67);
-            for (int i = 0; i < code.length; i++) {
+            for (int i = 0; i < code.length(); i++) {
                 // System.out.println("char at " + i + " index is: " + ch[i]);
                 mmOutputStream.write((char) code[i]);// d1-dk
             }
